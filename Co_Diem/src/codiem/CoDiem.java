80a124f6dc1d63ec6edad3c7272d492a86534e57
Hoang Nguyen <nguyenphihoangbg@gmail.com>
2017-11-13 06:18:13 +0700

    Realizable Move

--- Co_Diem/src/codiem/CoDiem.java
+++ Co_Diem/src/codiem/CoDiem.java
@@ -5,18 +5,47 @@
  */
 package codiem;
 
+import component.Point;
+
 /**
  *
  * @author nguyen
  */
 public class CoDiem {
-    static int[][] BroadChess = new int[8][8];
+    static byte[][] broadChess = new byte[8][8];
+    static Point[] white = new Point[6];
+    static Point[] black = new Point[6];
+
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[][] BroadChess = new int[8][8];
+        byte[][] BroadChess = new byte[8][8];
+        byte u = 0;
+        byte i = (byte) (u + 1);
         
+    }
+    
+    public void updateStatus(){
+        for(int i=1; i <= 8; i++)
+        {
+          for(int j = 1; j <= 8; j++)
+          {
+              broadChess[i][j] = 0;
+          }  
+        }
+        
+        for(int i = 0; i < 6; i++){
+            if( (white[i].getX_value() != 0) || (white[i].getY_value() != 0) )
+            {
+                broadChess[white[i].getX_value()][white[i].getY_value()] = 1; 
+            }
+             if( (black[i].getX_value() != 0) || (black[i].getY_value() != 0) )
+             {
+                 broadChess[black[i].getX_value()][black[i].getY_value()] = 2;
+             }   
+        }
     }
     
 }
--- Co_Diem/src/component/Point.java
+++ Co_Diem/src/component/Point.java
@@ -10,29 +10,91 @@ package component;
  * @author nguyen
  */
 public class Point {
-    short x_value;
-    short y_value;
+    byte x_value;
+    byte y_value;
 
-    public Point(short x_value, short y_value) {
+    public Point(byte x_value, byte y_value) {
         this.x_value = x_value;
         this.y_value = y_value;
         
     }
 
-    public short getX_value() {
+    public byte getX_value() {
         return x_value;
     }
 
-    public short getY_value() {
+    public byte getY_value() {
         return y_value;
+       
     }
 
-    public void setX_value(short x_value) {
+    public void setX_value(byte x_value) {
         this.x_value = x_value;
     }
 
-    public void setY_value(short y_value) {
+    public void setY_value(byte y_value) {
         this.y_value = y_value;
+    }
+    
+    public Point[] realizableMove(byte[][] broardChess){
+        Point[] Moves = new Point[8];
+        byte camp = broardChess[x_value][y_value];
+        byte index = 0;
+        if( (broardChess[x_value+1][y_value] == 0) && x_value+2 <= 8){
+            if((y_value - 1 >= 1) && (broardChess[x_value+2][y_value-1] != camp)){
+               Point k = new Point( (byte)(x_value+ 2) , (byte) (y_value-1));
+               Moves[index] = k;
+               index++;
+            } 
+            if((y_value + 1 <= 8) && (broardChess[x_value+2][y_value+1] != camp)){
+               Point k = new Point( (byte)(x_value+ 2) , (byte) (y_value+1));
+               Moves[index] = k;
+               index++;
+            } 
+        }
+        
+        if( (broardChess[x_value-1][y_value] == 0) && x_value-2 >= 1 ){
+            if((y_value - 1 >= 1) && (broardChess[x_value-2][y_value-1] != camp)){
+               Point k = new Point( (byte)(x_value - 2) , (byte) (y_value-1));
+               Moves[index] = k;
+               index++;
+            } 
+            if((y_value + 1 <= 8) && (broardChess[x_value-2][y_value+1] != camp)){
+               Point k = new Point( (byte)(x_value - 2) , (byte) (y_value+1));
+               Moves[index] = k;
+               index++;
+            } 
+        }
+        
+        if( (broardChess[x_value][y_value-1] == 0) && y_value-2 <= 8  ){
+            if((x_value - 1 >= 1) && (broardChess[x_value-1][y_value-2] != camp)){
+               Point k = new Point( (byte)(x_value-1) , (byte) (y_value-2));
+               Moves[index] = k;
+               index++;
+            }
+            
+            if((x_value + 1 <= 8) && (broardChess[x_value+1][y_value-2] != camp)){
+               Point k = new Point( (byte)(x_value+1) , (byte) (y_value-2));
+               Moves[index] = k;
+               index++;
+            } 
+            
+        }
+        
+        if( (broardChess[x_value][y_value+1] == 0) && y_value+2 <= 8  ){
+            if((x_value + 1 <= 8) && (broardChess[x_value+1][y_value+2] != camp)){
+               Point k = new Point( (byte)(x_value+ 1) , (byte) (y_value+2));
+               Moves[index] = k;
+               index++;
+            } 
+            
+            if((x_value - 1 >= 1) && (broardChess[x_value-1][y_value+2] != camp)){
+               Point k = new Point( (byte)(x_value-1) , (byte) (y_value+2));
+               Moves[index] = k;
+               index++;
+            } 
+        }
+        return Moves;
     }
     
     
